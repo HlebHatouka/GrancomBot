15f1f8a25d6f9623babd571ad4be8923698fe6b5
HlebHatouka <46506561+HlebHatouka@users.noreply.github.com>
2019-08-07 04:04:21 +0300

    Update grancombot.py

--- grancombot.py
+++ grancombot.py
@@ -1,7 +1,9 @@
 import telebot
 from telebot import types
+import sqlite3
+#import mysql.connector
 
-bot = telebot.TeleBot('token')
+bot = telebot.TeleBot('942160466:AAF2PUbUMAvlWpODUw9qdinDlG9NMChC09M')
 language = str()
 
 @bot.message_handler(commands=['start'])
@@ -18,7 +20,7 @@ def start_message(message):
     key_rus = types.InlineKeyboardButton(text='Русский', callback_data='rus')
     keyboard.add(key_rus)
     bot.send_message(chat_id=message.chat.id, text=greeting, reply_markup=keyboard)
-    
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     global language
@@ -49,26 +51,47 @@ class Helping:
     def __init__(self, lang, message):
         self.__lang = lang
         self.__message = message
-    
+        self.__table = "questions.sqlite"
+
     def help(self, message):
         self.__set_message(message)
-        if self.__lang == "eng":   
-            self.__eng_help()
-        elif self.__lang == "bel":  
-            self.__bel_help()
+        connection = self.__create_connection(self.__table)
+        if self.__lang == "eng":
+            self.__eng_help(connection)
+        elif self.__lang == "bel":
+            self.__bel_help(connection)
         elif self.__lang == "rus":
-            self.__rus_help()
+            self.__rus_help(connection)
+        self.__dissable_connection(connection)
         self.__get_command()
 
     def __set_message(self, message):
         self.__message = message
         return self.__message
-    
+
     def __get_command(self):
         bot.register_next_step_handler(self.__message, self.__get_number)
 
     def __stop_command (self):
         bot.clear_step_handler_by_chat_id(chat_id=self.__message.chat.id)
+
+    def __create_connection(self, path):
+        return sqlite3.connect(path)
+
+    def __get_questions(self, connection, name):
+        table_cursor = connection.cursor()
+        table_cursor.execute("SELECT question FROM {0}".format(name))
+        results = table_cursor.fetchall()
+        return results
+
+    def __get_answer(self, connection, name, number):
+        table_cursor = connection.cursor()
+        table_cursor.execute("SELECT answer FROM {0} WHERE id = {1}".format(name, number))
+        results = table_cursor.fetchall()
+        return results
+
+    def __dissable_connection(self, connection):
+        connection.close()
 
     @bot.message_handler(content_types=["text"])
     def __get_number(self, message):
@@ -78,182 +101,55 @@ class Helping:
                 self.__stop_command()
                 del self
                 return
-            if  int(self.__message.text) <= 7:
-                number = int(self.__message.text) 
-                if number == 1:
-                    self.first()
-                elif number == 2:
-                    self.second()
-                elif number == 3:
-                    self.third()
-                elif number == 4:
-                    self.fourth()
-                elif number == 5:
-                    self.fifth()
-                elif number == 6:
-                    self.sexth()
-                elif number == 7:
-                    self.seventh()
+            if 0 < int(self.__message.text) <= 7:
+                number = int(self.__message.text)
+                self.__answer(number)
             else:
                 raise ValueError()
             self.__get_command()
         except ValueError:
             self.__error_occurred()
-        
+
     def __error_occurred(self):
         if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Enter the number of question or /exit to leave!")
+            bot.send_message(chat_id=self.__message.chat.id, text = "Enter the number of question or /exit to leave "+
+            "and ask your own question!")
         if self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Увядзіце нумар пытання або /exit , каб выйсці!")
+            bot.send_message(chat_id=self.__message.chat.id, text = "Увядзіце нумар пытання або /exit , каб выйсці "+
+            "і задаць сваё ўласнае пытанне!")
         if self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Введите номер вопроса или /exit , чтобы выйти!")
+            bot.send_message(chat_id=self.__message.chat.id, text = "Введите номер вопроса или /exit , чтобы выйти "+
+            "и задать свой собственный вопрос!")
         self.__get_command()
-        
-    def __eng_help(self):
+
+    def __eng_help(self, connection):
+        questions = self.__get_questions(connection, self.__lang)
         bot.send_message(chat_id=self.__message.chat.id, text = 'I can help you to find the answers to some questions!\n'
-        'Enter the number of question that interests you.\n\n'
-        '1. What scooters can I buy?\n'
-        '2. What documents do you give?\n'
-        '3. Where are you at, where to see motorcycles?\n'
-        '4. Do you give a guarantee for the motorcycles that you sell?\n'
-        '5. Can I order equipment that you do not have on the site and how long to wait for delivery from Japan?\n'
-        '6. Can you help buy a new or restore an old battery for a Yamaha electric scooter?\n'
-        '7. Than used (second hand) Japanese electric scooters than new Chinese ones?\n\n'
-        'Enter the command /exit to leave choice menu and ask your own question.')
+        'Enter the number of question that interests you.\n\n')
+        for question in questions:
+            bot.send_message(chat_id=self.__message.chat.id, text = question)
+        bot.send_message(chat_id=self.__message.chat.id, text = 'Enter the command /exit to leave choice menu and ask your own question.')
 
-    def __bel_help(self):
+    def __bel_help(self, connection):
+        questions = self.__get_questions(connection, self.__lang)
         bot.send_message(chat_id=self.__message.chat.id, text = 'Я дапамагу Вам знайсці адказы на некаторыя пытанні!\n'
-        'Увядзіце нумар пытання, што Вас цікавіць.\n\n'
-        '1. Якія скутары ў вас можна набыць?\n'
-        '2. Якія дакуманты вы даяце?\n'
-        '3. Дзе вы знаходзіцеся, дзе паглядзець мотатэхніку\n'
-        '4. Ці даяце вы гарантыю на мотатэніку, якую прадаяце?\n'
-        '5. Ці можна ў вас замовіць тэхніку, якой у вас няма на сайце і як доўга чакаць дастаўкі з Японіі\n'
-        '6. Ці можаце вы дапамагчы набыць новую або аднавіць старую батарэю для электраскутара Yamaha?\n'
-        '7. Чым японскія электраскутары б/у лепшыя за новых кітайскіх?\n\n'
-        'Увядзіце каманду /exit , каб пакінуць выбар і задаць ўласнае пытанне.')
+        'Увядзіце нумар пытання, што Вас цікавіць.\n\n')
+        for question in questions:
+            bot.send_message(chat_id=self.__message.chat.id, text = question)
+        bot.send_message(chat_id=self.__message.chat.id, text = 'Увядзіце каманду /exit , каб пакінуць выбар і задаць ўласнае пытанне.')
 
-    def __rus_help(self):
+    def __rus_help(self, connection):
+        questions = self.__get_questions(connection, self.__lang)
         bot.send_message(chat_id=self.__message.chat.id, text = 'Я помогу Вам найти ответы на некоторые вопросы!\n'
-        'Введите номер интересующего Вас вопроса.\n\n'
-        '1.  Какие скутеры у вас можно купить?\n'
-        '2.  Какие документы вы даёте?\n'
-        '3.  Где вы находитесь, где посмотреть мототехнику?\n'
-        '4.  Даёте ли вы гарантию на мототехнику, которую продаёте?\n'
-        '5.  Можно ли у вас заказать технику, которой у вас нет на сайте и как долго ждать доставки из Японии?\n'
-        '6.  Можете ли вы помочь купить новую или восстановить старую батарею для электроскутера Yamaha?\n'
-        '7.  Чем японские электроскутеры б/у лучше новых китайских?\n\n'
-        'Введите команду /exit , чтобы оставить выбор и задать собственный вопрос.')
+        'Введите номер интересующего Вас вопроса.\n\n')
+        for question in questions:
+            bot.send_message(chat_id=self.__message.chat.id, text = question)
+        bot.send_message(chat_id=self.__message.chat.id, text = 'Введите команду /exit , чтобы оставить выбор и задать собственный вопрос.')
 
-    def first(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "GRANCOM company sells only high-quality "+
-            "scooters and motorcycles of Japanese brands brought directly from the Japanese market.")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Кампанія ГРАНКАМ прадае толькі якасныя "+
-            "скутары і матацыклы японскіх брэндаў, прывезеныя непасрэдна з японскага рынку.")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Компания ГРАНКОМ продает только качественные "+
-            "скутеры и мотоциклы японских брендов, привезенные непосредственно с японского рынка.")
+    def __answer(self, number):
+        connection = self.__create_connection(self.__table)
+        answer = self.__get_answer(connection, self.__lang, number)
+        bot.send_message(chat_id=self.__message.chat.id, text = answer)
+        self.__dissable_connection(connection)
 
-    def second(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "When selling, the buyer receives all the necessary "+
-            "documents for registration with the traffic police (if required), "+
-            "namely: the purchase agreement, the original customs declaration, "+
-            "vehicle safety certificate and invoice statement for motorcycle equipment "+
-            "with engines of more than 100 cubic centimeters.")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Пры продажу пакупнік атрымоўвае ўсе неабходныя "+
-            "дакуманты для пастаноўкі на ўлік у ДАІ (калі яна патрабуецца), "+
-            "а менавіта: дамова куплі-продажу, арыгінальную мытную дэкларацыю, "+
-            "пасведчанне бяспекі транспартнага сродка і на мотатэхніку "+
-            "з рухавікамі не больш за 100 куб. см рахунак-даведку.")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "При продаже покупатель получает все необходимые "+ 
-            "документы для постановки на учет в ГАИ (если она требуется), "+
-            "а именно: договор купли-продажи, оригинальную таможенную декларацию, "+
-            "свидетельство безопасности транспортного средства и на мототехнику "+
-            "с двигателями более 100 куб. см счет-справку.")
-
-    def third(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "At the moment, the warehouses are located in Minsk "+
-            "on the territory of the Belhashand (Belhoztorg) base on Platonov st., 34.")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "На дадзены момант склады знаходзяцца ў г. Мінску "+
-            "на тэрыторыі базы «Белгасганд» па вул. Платонава, 34.")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "На данный момент склады находятся в г. Минске "+
-            "на территории базы «Белхозторг» по ул. Платонова, 34.")
-
-    def fourth(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Yes, we sell equipment in good condition, "+
-            "prepared for sale and give a guarantee for hidden defects "+
-            "that are not revealed. The terms of the guarantee are stated in the sales contract.")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Так, мы прадаем тэхніку ў належным стане, "+
-            "падрыхтаваную да продажу і даем гарантыю на схаваныя "+
-            "дэфекты, якія не выявілі. У дамове куплі-продажу "+
-            "прапісаныя ўмовы гарантыі.")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Да, мы продаем технику в надлежащим состоянии, "+
-            "подготовленную к продаже и даем гарантию на скрытые "+
-            "дефекты, которые не выявили. В договоре купли-продажи "+
-            "прописаны условия гарантии.")
-
-    def fifth(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Yes. You can order any equipment from the Japanese market. "+
-            "Delivery takes approximately 70 days after shipment "+
-            "(50 days the container goes to the port of Klaipeda, is delivered to "+
-            "Minsk within a week and undergoes customs procedures, then arrives at "+
-            "the company's warehouse, is checked, then the experts of the accredited "+
-            "organization inspect and check the vehicles and issue safety certificates. "+
-            "After that we give it to the customer).")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Так. можна замовіць любую тэхніку з японскага рынку. "+
-            "Дастаўка займае прыкладна на працягу 70 дзён пасля адгрузкі "+
-            "(50 дзён кантэйнер ідзе да порта Клайпеды, цягам тыдня дастаўляецца "+
-            "ў Мінск і праходзе мытныя працэдуры, затым адмыслоўцы "+
-            "акрэдытаванай арганізаціі аглядаюць і правяраюць транспартныя "+
-            "сродкі і выдаюць пасведчанне бяспекі. Пасля гэтага мы аддаем заказніку).")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Да. Можно заказать любую технику с японского рынка. "+
-            "Доставка занимает примерно в течении 70 дней после отгрузки "+
-            "(50 дней контейнер идет до порта Клайпеды, в течении недели "+
-            "доставляется в Минск и проходит таможенные процедуры, затем "+
-            "прибывает на склад компании, проходит проверку, затем специалисты "+
-            "аккредитованной организации осматривают и проверяют транспортные "+
-            "средства и выдают свидетельства безопасности. После этого мы отдаем заказчику).")
-
-    def sexth(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Yes, we can help to buy any spare parts for "+
-            "equipment that we sell, including batteries for electric "+
-            "scooters, we can also replace elements in the old equipment, "+
-            "it will be three times cheaper than buying a new one.")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Так, мы можам дапамагчы набыць любыя запасныя "+
-            "часткі тэхнікі, якую прадаем, у тым ліку і батарэі "+
-            "для электраскутараў; таксама можам замяніць элементы "+
-            "у старой, што будзе ўтрая таней, чым набыць новую.")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Да, мы можем помочь купить любые запасные "+
-            "части к технике, которую продаем, в том числе и "+
-            "батареи для электроскутеров, также можем заменить "+
-            "элементы в старой, что будет втрое дешевле чем купить новую.")
-
-    def seventh(self):
-        if self.__lang == "eng":
-            bot.send_message(chat_id=self.__message.chat.id, text = "You can get more information about Japanese "+
-            "electric scooters on our separate website http://electroskuter.by/")
-        elif self.__lang == "bel":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Больш інфармацыі па японскім электраскутарам "+
-            "вы можаце знайсці на нашым асобным сайце http://electroskuter.by/")
-        elif self.__lang == "rus":
-            bot.send_message(chat_id=self.__message.chat.id, text = "Больше информации по японским электроскутерам "+
-            "вы можете получить на отдельном нашем сайте http://electroskuter.by/")
-            
 bot.polling(none_stop=True)
